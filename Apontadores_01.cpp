c8ce2e367e322c3d6ced1da3ca42699415349ea4
Domingos Ribeiro <domingos.ribeiro@gmail.com>
2021-01-22 00:16:15 +0000

    Add project files.

---
+++ Apontadores_01.cpp
@@ -0,0 +1,35 @@
+
+#include <iostream>
+using namespace std;
+#include<time.h>
+
+
+struct ficha
+	{
+		string nome;
+		int idade;
+	};
+
+void geravalor()
+	{
+
+	}
+
+void init_array()
+	{
+
+	}
+
+void print_array()
+	{
+
+	}
+
+
+int main()
+{
+
+	init_array();
+	print_array();
+}
+
